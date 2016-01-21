d71aa6f4c1b1b1f51bb03da729201b4828db41de
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-01-21 11:46:48 -0500

    Fooling around with variables and 'if' statements.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,17 @@
+"use strict";
+
 var myName = "Devon";
+var myAge = 34;
 
 console.log("App Started . . . " + myName);
+console.log(myName + " is " + myAge + ".");
+
+if (myAge > 40) {
+    console.log("You are old and wise.");
+} else {
+    console.log("You are young and strong (and maybe dumb).");
+}
+
+if (myName === "Devon") {
+    console.log("Hello " + myName);
+}
